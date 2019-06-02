3250d7bf34ce2550d6e210ecc38df9c396d6076c
Robson <me@rsilva.net>
2019-06-02 02:20:41 -0300

    Ugly exception handling removed

--- ThriftHelper.NET/ThriftCmd.cs
+++ ThriftHelper.NET/ThriftCmd.cs
@@ -10,31 +10,24 @@ namespace ThriftHelper.NET
     {
         public DirectoryModel Execute(Language language, string thriftIDLCode)
         {
-            try
-            {
-                string formattedLanguage = Formatter.FormatLanguage(language);
-                string guid = Guid.NewGuid().ToString();
-                string tempPath = Path.GetTempPath();
-                string tempPathOut = Path.Combine(tempPath, guid, "Out");
-                string tempPathMap = Path.Combine(tempPath, guid, "Map");
-                string mapFilePath = Path.Combine(tempPathMap, "Code.thrift");
-                string arguments = $"--gen \"{formattedLanguage}\" -out \"{tempPathOut}\" \"{mapFilePath}\"";
+            string formattedLanguage = Formatter.FormatLanguage(language);
+            string guid = Guid.NewGuid().ToString();
+            string tempPath = Path.GetTempPath();
+            string tempPathOut = Path.Combine(tempPath, guid, "Out");
+            string tempPathMap = Path.Combine(tempPath, guid, "Map");
+            string mapFilePath = Path.Combine(tempPathMap, "Code.thrift");
+            string arguments = $"--gen \"{formattedLanguage}\" -out \"{tempPathOut}\" \"{mapFilePath}\"";
 
-                Directory.CreateDirectory(tempPathMap);
-                Directory.CreateDirectory(tempPathOut);
-                File.WriteAllText(mapFilePath, thriftIDLCode);
+            Directory.CreateDirectory(tempPathMap);
+            Directory.CreateDirectory(tempPathOut);
+            File.WriteAllText(mapFilePath, thriftIDLCode);
 
-                ThriftExe.Execute(arguments);
+            ThriftExe.Execute(arguments);
 
-                var directoryModel = new DirectoryModel(guid);
-                directoryModel.FillFromFileSystem(tempPathOut);
+            var directoryModel = new DirectoryModel(guid);
+            directoryModel.FillFromFileSystem(tempPathOut);
 
-                return directoryModel;
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+            return directoryModel;
         }
     }
 }
